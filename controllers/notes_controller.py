1c37cb0908d106a49a02081510b2c5cde38130c4
Juan Vargas <juandeveloper19@gmail.com>
2024-05-03 23:51:58 -0500

    Added the creation controller for notes. This controller creates, retrieves, and returns the created note

--- controllers/notes_controller.py
+++ controllers/notes_controller.py
@@ -1,5 +1,15 @@
+from bson import ObjectId
+from fastapi.encoders import jsonable_encoder
 from database.connection import studyhub_database
+from schemas.notes_schema import Note
 
-
-# Collection for the notes
+# Collection for the notes.
 notes_collection = studyhub_database.notes
+
+# Creates a new note in the database, retrieves it, and returns the created note.
+async def create_note(note_to_create: Note):
+  note_to_create.id = str(ObjectId())
+  encoded_note = jsonable_encoder(note_to_create)
+  insertion_result = await notes_collection.insert_one(encoded_note)
+  retrieved_note = await notes_collection.find_one({"_id": insertion_result.inserted_id})
+  return retrieved_note
