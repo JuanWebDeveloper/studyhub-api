da5f54b56c5757d1a2fc6d85ca08c908344fc74c
Juan Vargas <juandeveloper19@gmail.com>
2024-05-03 23:39:54 -0500

    Refactor the PyObjectIdValidator class to improve code readability and adhere to PEP 8 style guidelines

--- utils/objectid_validator.py
+++ utils/objectid_validator.py
@@ -2,12 +2,11 @@ from bson import ObjectId
 
 # Custom validator for Pydantic models that validates and converts ObjectId values to strings.
 class PyObjectIdValidator(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v, values):
-        if not ObjectId.is_valid(v):
-            raise ValueError('Invalid ObjectId')
-        return str(v)
+  @classmethod
+  def __get_validators__(cls):
+      yield cls.validate  
+  @classmethod
+  def validate(cls, v, values):
+    if not ObjectId.is_valid(v):
+      raise ValueError('Invalid ObjectId')
+    return str(v)
