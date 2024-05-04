bfc0b091d370dd31395e2053fe1a7055676ce546
Juan Vargas <juandeveloper19@gmail.com>
2024-05-04 18:46:45 -0500

    Implemented the route handler responsible for the retrieval of all notes within the notes router

--- routes/notes_route.py
+++ routes/notes_route.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, HTTPException
 from bson import ObjectId
-from controllers.notes_controller import create_notes, get_one_notes
+from controllers.notes_controller import create_notes, get_all_notes
 from schemas.notes_schema import Note
 
 
 # Create a new APIRouter instance to handle note-related routes.
 notes_router = APIRouter()
 
-# Define a route to create a new note.
+# Define a POST route '/create-note' to create a new note in the database.
 @notes_router.post("/create-note")
 async def save_notes(new_note: Note):	
   new_note_dict = new_note.model_dump()
@@ -18,3 +18,9 @@ async def save_notes(new_note: Note):
   if response:
     return response
   raise HTTPException(status_code=400, detail="Note creation failed. Please check the input data and try again.")
+
+# Define a GET route '/all-notes' to retrieve all notes from the database.
+@notes_router.get("/all-notes")
+async def fetch_all_notes():
+  all_notes = await get_all_notes()
+  return all_notes
